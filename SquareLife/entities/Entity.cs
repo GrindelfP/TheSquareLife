3192bc8157f1cb0dc709b24223c1fff32616ae81
Grindelf <grindelf.perlomutrovij@gmail.com>
2022-05-11 14:06:14 +0300

    Add project files.

---
+++ SquareLife/entities/Entity.cs
@@ -0,0 +1,18 @@
+﻿namespace SquareLife
+{
+    internal abstract class Entity
+    {
+        protected bool Status { get; set; }
+        protected string Id { get; set; } 
+        public virtual void Appear() { }
+        public virtual void Move() { }
+        public virtual void GiveBirth() { }
+        public virtual void Swallow() { }
+        public virtual void Disappear() { }
+        public Entity(bool status, string id)
+        {
+            Status = status;
+            Id = id;
+        }
+    }
+}
---
+++ SquareLife/entities/Kuvat.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SquareLife
+{
+    internal class Kuvat : Entity
+    {
+        public Kuvat(bool status, string id) : base(status, id) { }
+    }
+}
